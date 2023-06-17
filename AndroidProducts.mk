a29eca03b5f25631fbfa094810515d23d7a56939
iEatTics <aaronrector09@gmail.com>
2023-06-17 13:43:57 -0500

    first

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Phoenix_7_KD.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Phoenix_7_KD-user \
+    omni_Phoenix_7_KD-userdebug \
+    omni_Phoenix_7_KD-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_Phoenix_7_KD-user
+add_lunch_combo omni_Phoenix_7_KD-userdebug
+add_lunch_combo omni_Phoenix_7_KD-eng
